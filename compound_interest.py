1a445db7292b9edabd754f90b88f99b0f8073572
Mani8247 <gollapallimanikumar7@gmail.com>
2023-03-30 20:45:52 +0530

    done

---
+++ compound_interest.py
@@ -0,0 +1,6 @@
+pri = int(input())
+rad = int(input())
+n = int(input())
+res = pri*((1+rad//100))**n
+print(res)
+
---
+++ list_practise.py
@@ -0,0 +1,3 @@
+list = input("enter a list:")
+for i in range(len(list)):
+    print(list[i],end=" ")
